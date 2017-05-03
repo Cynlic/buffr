88a502add53fe7ccf63c7cc61997ef9acf7b06f5
Harrison Montgomery <harrison.s.montgomery@gmail.com>
2017-05-02 22:57:59 -0400

    Now the microphone output works. It is implemented with an atom and allows stateful manipulation. The atomic method implicitly fixes a Firefox-only bug, which cuts off microphone audio due to garbage collection.

--- src/buffr/core.cljs
+++ src/buffr/core.cljs
@@ -37,6 +37,8 @@
 
 (def gain (.createGain ctx)) ;; a volume node! for volume!
 
+(def a-mic (atom 0))  ;; this is an atom which evetually is assigned the value of the user's microphone MediaStreamSource object. It is needed to prevent garbage collection issues with Firefox.
+
 (defonce white-noise-buffer (js/Float32Array. (into-array (repeatedly length #(rand 1)))))
 ;; all AudioBuffers must implement the Float32Array interface.
 ;; This array contains 22050 samples of random values between 0 and 1.
@@ -46,23 +48,41 @@
   [buf sample-vec]
   (.copyToChannel buf sample-vec 0))
 
-(fill-buf buf white-noise-buffer) ;;Imperatively fill the buffer
+(fill-buf buf white-noise-buffer) ;; Imperatively fill the buffer
 
 (set! (.-buffer audio-source) buf) ;; Set the AudioBufferSource.buffer to our buffer
 
 (set! (.-loop audio-source) true) ;; Set the loop conditional to true (it is natively false)
 
-; (.connect audio-source gain)
+(.connect gain (.-destination ctx))
 
-; (.connect audio-source (.-destination ctx)) ;; Connect the audio graph
+(set! (.-value (.-gain gain)) 1)
 
+(defn mic-connect "This function just patches any audio-in to the gain. Because why not?"
+  [mic-node] (.connect mic-node gain))
 
-(defn mic-connect [mic-node] (.connect mic-node (.-destination ctx)))
+(defn mic-handler
+  "Saves an atomic refrence to the microphone node to prevent garbage collection as well as connecting the reference to the output.This function is a callback, to be used on the js/Promise returned by getUserMedia. Once it is called, a-mic should be manipulatable as an audio node, but using atomic syntax."
+  [stream]
+  (reset! a-mic (.createMediaStreamSource ctx stream))
+  (mic-connect (deref a-mic)))
 
-(defn mic-handler [stream] (mic-connect (.createMediaStreamSource ctx stream)))
+(defn handler-test []
+   (println "hey, I'm being called!"))
 
-(js/navigator.mediaDevices.getUserMedia {:audio true} mic-handler js/console.warn)
-(.start audio-source) ;; hell yeah, we've got some sound
+(defn error-handler
+  "Callback to print any errors associated with the getUserMedia promise."
+  [error]
+  (println error))
+
+(defn promise-handler
+  "Provides some ease of use with the getUserMedia promise, wrapping the success and failure conditions."
+  [promise on-success]
+  (.catch (.then promise on-success) error-handler))
+
+(promise-handler (js/navigator.mediaDevices.getUserMedia
+                  (clj->js {:audio true})) mic-handler)
+;; Imperatively connect up the microphone. Move this into a reagent component as soon as possible.
 
 (defonce app-state (atom {:text "Hello world!"}))
 
